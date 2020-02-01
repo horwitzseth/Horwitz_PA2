23315cc491017d62ffdd7759fa4603fb86d67233
horwitzseth <59711763+horwitzseth@users.noreply.github.com>
2020-02-01 11:55:56 -0500

    Delete Application.java

--- Horwitz_p2/src/Application.java
+++
@@ -1,8 +0,0 @@
-public class Application {
-    public static void main(String[] args) {
-        BMICalculator app = new BMICalculator();
-        app.readUserData();
-        app.calculateBmi();
-        app.displayBmi();
-    }
-}
